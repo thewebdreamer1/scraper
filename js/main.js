9c277acd7de1f6eedc493ebdba8deb3d4a8b6642
thewebdreamer1 <thewebdreamer@gmail.com>
2019-07-21 18:42:16 -0700

    Attempt 4

--- js/main.js
+++ js/main.js
@@ -1,11 +1,5 @@
 $( function(){
-  $(".prez_block").click(function(){
-    var name = $(this).find(".name").text().toLowerCase().replace(/\s/g, '').replace(/\./g,'');
-    $(".modal-for-" + name).show();
-  });
-  $(".modal").click(function(){
-    $(this).hide();
-  });
+
 });
 
 $.getJSON( "output.json", function( data ) {
@@ -17,5 +11,12 @@ $.getJSON( "output.json", function( data ) {
     var term = data[key].term.replace('In office','In Office: ');
     wrapper.append( "<div class='prez_block' data-modal='" + modalName + "' style='background-image:url(" + data[key].image + ")'><div class='overlay'></div><div class='content'><p class='name'>" + data[key].name + "</p></div></div>" );
     wrapper.append( "<div class='modal modal-for-" + modalName + "'><div class='modal-content'><p class='modal-name'>" + data[key].name + "</p><p class='modal-birthday'>Birthday: " + format + "</p><p class='modal-term'>" + term + "</p></div></div>" );
+  });
+  $(".prez_block").click(function(){
+    var name = $(this).find(".name").text().toLowerCase().replace(/\s/g, '').replace(/\./g,'');
+    $(".modal-for-" + name).show();
+  });
+  $(".modal").click(function(){
+    $(this).hide();
   });
 });
