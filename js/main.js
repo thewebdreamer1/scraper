a3d2f2f11905d092774b3b05107123c07ff069fd
thewebdreamer1 <thewebdreamer@gmail.com>
2019-07-21 18:59:40 -0700

    Minor fix for term

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ $.getJSON( "output.json", function( data ) {
     var modalName = data[key].name.toLowerCase().replace(/\s/g, '').replace(/\./g,'');
     var date = data[key].birthday;
     var format = $.datepicker.formatDate('MM dd, yy', new Date(date));
-    var term = data[key].term.replace('In office','In Office: ');
+    var term = data[key].term.replace('In office','In Office: ').replace('[a]','');
     wrapper.append( "<div class='prez_block' data-modal='" + modalName + "' style='background-image:url(" + data[key].image + ")'><div class='overlay'></div><div class='content'><p class='name'>" + data[key].name + "</p></div></div>" );
     wrapper.append( "<div class='modal modal-for-" + modalName + "'><div class='modal-content'><p class='modal-name'>" + data[key].name + "</p><p class='modal-birthday'>Birthday: " + format + "</p><p class='modal-term'>" + term + "</p></div></div>" );
   });
